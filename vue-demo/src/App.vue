7d9e64603042b92533d8afbad5e6f19568aba1f5
ching7 <775608598@qq.com>
2020-06-12 00:05:31 +0800

    add: router demo

---
+++ vue-demo/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-link to="/hello">to hello</router-link>
+    <router-link to="/">to main page</router-link>
+    <!-- 路由视图 -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-demo/src/components/Hello.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>hello vue {{name}}</h1>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: "张三"
+    };
+  }
+};
+</script>
+<style>
+</style>
