096511ef556e17a1f0f75525da278acf1f6a4849
ching7 <775608598@qq.com>
2020-06-16 23:05:13 +0800

    add:增加elemenui

--- vue-demo/src/components/Hello.vue
+++ vue-demo/src/components/Hello.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
     <h1>hello vue {{name}}</h1>
+    <el-radio v-model="radio" label="1">备选项1</el-radio>
+    <el-radio v-model="radio" label="2">备选项2</el-radio>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      name: "张三"
+      name: "张三",
+      radio: 1
     };
   }
 };
--- vue-demo/src/router/index.js
+++ vue-demo/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Hello from '@/components/Hello'
+import Mytable from '@/components/Mytable'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/hello',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/table',
+      name:'Mytable',
+      component: Mytable
     }
   ]
 })
