4fc1681ab118d386711c1b77441040164b919e09
chenyn22577 <chenyn22577@hundsun.com>
2020-09-25 10:07:09 +0800

    fix:update resources

--- vue-demo/src/components/Hello.vue
+++ vue-demo/src/components/Hello.vue
@@ -3,16 +3,29 @@
     <h1>hello vue {{name}}</h1>
     <el-radio v-model="radio" label="1">备选项1</el-radio>
     <el-radio v-model="radio" label="2">备选项2</el-radio>
+    <button @click='hello'>test $</button>
   </div>
 </template>
 <script>
 export default {
-  data() {
+  name: 'mycom',
+  data () {
     return {
       name: "张三",
       radio: 1
     };
-  }
+  },
+  methods: {
+    hello () {
+      this.$myMethods('hello')
+    },
+    myMethods (msg) {
+      alert(msg)
+    }
+  },
+  created () {
+    this.$myMethods = this.myMethods
+  },
 };
 </script>
 <style>
